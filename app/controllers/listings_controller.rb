f501aebe2e9fe3cc0f63baf94f6c26f8903f75b3
Crystal <crystalcontrerasgrossman@gmail.com>
2015-03-26 11:32:35 -0700

    Fixed controller to and added css styling to flash messages

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,3 +1,27 @@
 class ListingsController < ApplicationController
   before_filter :authenticate_user!, except: [:index, :show]
+
+  def index
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @user = User.find(current_user.id)
+    @listing = @user.listings.new(listing_params)
+    if @listing.save
+      flash[:success] = "Listing Successfully Added!"
+      redirect_to root_path
+    else
+      flash[:danger] = "There was a problem creating your listing, please try again."
+      render :new
+    end
+  end
+
+private
+  def listing_params
+    params.require(:listing).permit(:price, :location, :property_type)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
-  resources :listings
+  resources :listings, except: [:show, :index]
 end
